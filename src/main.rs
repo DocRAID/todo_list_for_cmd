e53a7668f72bb92900aed6a5a255c77edefa8a8a
l050101 <l01062506145@gmail.com>
2022-10-17 00:28:43 +0900

    fid

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{path::PathBuf, fmt::Error};
+use std::{path::PathBuf, fmt::Error, fs::File};
 
 fn main() {
     let path = Setting::new();
@@ -15,11 +15,22 @@ impl Setting {
         document_path.push("todo.txt");
         Ok(document_path)
     }
+
     fn show_cmd(){
         println!("welcome to Todo List!");
         println!("add [your todo] : input your Todos");
         println!("rm [todo's id] : delete your Todos");
         println!("ls : show your Todos");
         println!("clear : clear Todos.");
+    }
+
+    fn file_exist(path:PathBuf) -> Result<(),Error>{ //파일이 존재하는지 확인.
+        let exits = path.exists();
+    
+        if !exits {
+            println!("created file!");
+            File::create(path).expect("Setting Err: file create failed");
+        }
+        Ok(())
     }
 }
